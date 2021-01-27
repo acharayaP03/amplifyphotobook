2e0e3989f4155dae3638be684c34fee988ff21d7
acharyaP03 <acharyap03@gmail.com>
2021-01-27 21:04:09 +1100

    Vuex auth store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="block m-auto home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
