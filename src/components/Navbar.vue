e15c78228e5a4ca0712c16d89dd2aca226c5a21a
acharyaP03 <acharyap03@gmail.com>
2021-02-01 21:21:32 +1100

    Sign up and authentication

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <ul class="flex justify-end">
+      <li class="mr-6" v-if="!user">
+        <router-link to="/">
+          <a href="" class="cursor-pointer text blue-500 hover:text-blue-800">Login</a>
+        </router-link>
+      </li>
+      <li class="mr-6" v-if="!user">
+        <router-link to="/signup">
+          <a href="" class="cursor-pointer text blue-500 hover:text-blue-800">Sign up</a>
+        </router-link>
+      </li>
+      <li class="mr-6" v-if="user">
+        <div class="font-semibold text-black-500">
+          Welcome {{ user.username }}
+        </div>
+      </li>
+
+      <li class="mr-6" v-if="user">
+        <router-link to="/signup">
+          <a href="" class="cursor-pointer text blue-500 hover:text-blue-800">Albums</a>
+        </router-link>
+      </li>
+      <li class="mr-6" v-if="user">
+        <router-link to="/signup">
+          <a href="" class="cursor-pointer text blue-500 hover:text-blue-800">Logout</a>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  methods: {
+    async logout() {
+      await this.$store.dispatch('auth/logout');
+      this.$router.push('/');
+    },
+  },
+  computed: {
+    ...mapGetters({
+      user: 'auth/user',
+    }),
+  },
+};
+</script>
--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,10 +1,92 @@
+/* eslint-disable max-len */
 <template>
-  <div>Sign up page</div>
+  <div>
+    <form v-if="!confirmPassword" class="flex flex-col items-center" @submit.prevent="signup">
+      <div class="flex flex-col user">
+        <label for="username" class="label">Username: </label>
+        <input type="text" class="input_label" v-model="username" id="username">
+      </div>
+      <div class="flex flex-col mt-2 password">
+        <label for="username" class="label">Password: </label>
+        <input type="password" class="input_password" v-model="password" id="password">
+      </div>
+      <div class="flex flex-col mt-2 email">
+        <label for="username" class="label">Email: </label>
+        <input type="text" class="input_password" v-model="email" id="email">
+      </div>
+      <button class="btn-blue"> Sign Up</button>
+    </form>
+    <div class="text-red-600" v-if="error">
+      {{ error.message }}
+    </div>
+    <div v-if="confirmPassword" class="w-4/12 m-auto">
+      <h3>Enter your code. Please check your email</h3>
+      <div class="flex flex-col mt-2">
+        <label class="block text-sm font-bold text-gray-700" for="password">Code</label>
+        //
+        <input
+          class="input_confirm"
+          type="text"
+          v-model="code"
+        />
+        <button class="btn-blue" @click="confirmSignUp">Confirm Code</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-
+  name: 'SignUpPage',
+  data() {
+    return {
+      username: '',
+      password: '',
+      email: '',
+      error: '',
+      confirmPassword: '',
+      code: '',
+      message: '',
+    };
+  },
+  methods: {
+    // eslint-disable-next-line consistent-return
+    async signup() {
+      if (!this.email || !this.password) {
+        return false;
+      }
+      try {
+        await this.$store.dispatch('auth/signUp', {
+          username: this.username,
+          password: this.password,
+          email: this.email,
+        });
+        this.confirmPassword = true;
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    // eslint-disable-next-line consistent-return
+    async confirmSignUp() {
+      if (!this.username || !this.code) {
+        return;
+      }
+      try {
+        const { username, password, code } = this;
+        await this.$store.dispatch('auth/confirmSignUp', {
+          username,
+          code,
+        });
+        await this.$store.dispatch('auth/login', {
+          username,
+          password,
+        });
+        this.$router.push('/albums');
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
-
 </script>
